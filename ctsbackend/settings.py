d17e4a1275322500d29ca0b9a363f8ac09e9ce81
deepakdpyqaz <77318262+deepakdpyqaz@users.noreply.github.com>
2021-03-04 15:28:25 +0530

    privacy protection

--- ctsbackend/settings.py
+++ ctsbackend/settings.py
@@ -138,9 +138,9 @@ MEDIA_URL='/media/'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'connectheshelf@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'shelf@connect'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
